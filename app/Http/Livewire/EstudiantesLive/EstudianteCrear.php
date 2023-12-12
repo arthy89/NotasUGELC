2f12bb0ce65e43c0d00606d8e6294b0ab532b627
Arhyel Ramos Flores <arthy89>
2023-12-12 16:17:25 -0500

    Admin vigilancia add est

--- app/Http/Livewire/EstudiantesLive/EstudianteCrear.php
+++ app/Http/Livewire/EstudiantesLive/EstudianteCrear.php
@@ -28,6 +28,13 @@ class EstudianteCrear extends Component
                 'estudiante.est_grado' => 'required',
                 'estudiante.est_seccion' => 'required',
             ];
+        } else if ($this->usuario->rol == 'Admin' && $this->usuario->id_inst != 1) {
+            return [
+                'estudiante.est_apell' => 'required',
+                'estudiante.est_name' => 'required',
+                'estudiante.est_grado' => 'required',
+                'estudiante.est_seccion' => 'required',
+            ];
         } else if ($this->usuario->rol == 'Director') {
             return [
                 'estudiante.est_apell' => 'required',
@@ -50,6 +57,13 @@ class EstudianteCrear extends Component
                 'estudiante.est_apell.required' => 'Los APELLIDOS son requeridos',
                 'estudiante.est_name.required' => 'Los NOMBRES son requeridos',
                 'estudiante.id_inst.required' => 'Se requiere la INSTITUCIÓN',
+                'estudiante.est_grado.required' => 'Se requiere el GRADO',
+                'estudiante.est_seccion.required' => 'Se requiere la SECCIÓN',
+            ];
+        } else if ($this->usuario->rol == 'Admin' && $this->usuario->id_inst != 1) {
+            return [
+                'estudiante.est_apell.required' => 'Los APELLIDOS son requeridos',
+                'estudiante.est_name.required' => 'Los NOMBRES son requeridos',
                 'estudiante.est_grado.required' => 'Se requiere el GRADO',
                 'estudiante.est_seccion.required' => 'Se requiere la SECCIÓN',
             ];
@@ -77,7 +91,8 @@ class EstudianteCrear extends Component
     public function guardar_estudiante()
     {
         $this->validate();
-        if ($this->usuario->rol == 'Admin' && $this->usuario->id_inst == 1) {
+        if ($this->usuario->rol == 'Admin' && $this->usuario->id_inst != 1) {
+            $this->estudiante->id_inst = $this->usuario->id_inst;
         } else if ($this->usuario->rol == 'Director') {
             $this->estudiante->id_inst = $this->usuario->id_inst;
         } else if ($this->usuario->rol == 'Docente') {
